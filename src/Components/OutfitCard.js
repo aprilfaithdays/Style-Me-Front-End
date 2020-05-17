459d6275eea38f865bb1da7be521d62e47ebd87e
aprilfaithdays <aprilfaith.e@gmail.com>
2020-05-17 17:55:19 -0400

    rendered an outfit show page

--- src/Components/OutfitCard.js
+++ src/Components/OutfitCard.js
@@ -13,11 +13,8 @@ const OutfitCard = props => {
             {/* {console.log(outfit)} */}
             <Link to={`/outfits/${outfit.id}`}> Outfit Name: {outfit.name} <br/></Link>
             <img src={top.img_url} alt='top'/> <br/>
-            top Name: {top.name} <br/>
             <img src={bottom.img_url} alt='bottom'/> <br/>
-            bottom Name: {bottom.name} <br/>
             <img src={shoe.img_url} alt='shoe'/> <br/>
-            shoe Name: {shoe.name} <br/>
             Created by: {user.name}
         </p>
     )
--- src/Components/OutfitDetails.js
+++ src/Components/OutfitDetails.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const OutfitDetails = props => {
+    const id = parseInt(props.match.params.id,0)
+    const [outfit, setOutfit] = useState('')
+    const [user, setUser] = useState('')
+    const [top, setTop] = useState('')
+    const [bottom, setBottom] = useState('')
+    const [shoe, setShoe] = useState('')
+
+    useEffect(() => {
+        fetchOutfit()
+        // eslint-disable-next-line
+    }, [id])
+
+    const fetchOutfit = () => {
+        fetch(`http://localhost:3000/outfits/${id}`)
+        .then(res => res.json())
+        .then(res => {
+            setOutfit(res);
+            setUser(res.user);
+            setTop(res.top);
+            setBottom(res.bottom);
+            setShoe(res.shoe)
+        })
+    }
+
+    const outfitPrice = () => {
+        return top.price + bottom.price + shoe.price
+    }
 
     return(
         <div>
-            {console.log(props.match.params.id)}
-            OutfitDetails Component {props.match.params.id}
+            <h3>{outfit.name}</h3>
+            Created by: {user.name}<br/>
+            Price: ${outfitPrice()} <br/>
+            <img src={top.img_url} alt='top'/> <br/>
+            <img src={bottom.img_url} alt='bottom'/> <br/>
+            <img src={shoe.img_url} alt='shoe'/> <br/>
         </div>
     )
 }
