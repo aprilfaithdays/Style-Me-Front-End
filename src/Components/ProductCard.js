33165d87fad6eac45b655bd8178ea91d860afa80
aprilfaithdays <aprilfaith.e@gmail.com>
2020-05-17 18:31:19 -0400

    rendered show page

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ProductCard = () => {
+const ProductCard = props => {
+    const id = props.id
+    const category = props.category
+    const [product, setProduct] = useState('')
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/${category}/${id}`)
+        .then(res => res.json())
+        .then(res => setProduct(res))
+    }, [id, category])
+
     return(
         <div>
-            ProductCard Component
+            ProductCard Component<br/>
+            <img src={product.img_url} alt="product"/>
+            {console.log(product)}
         </div>
     )
 }
