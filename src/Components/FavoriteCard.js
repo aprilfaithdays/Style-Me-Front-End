9987c619cf345568155608e80dbdf7f9df37fe99
aprilfaithdays <aprilfaith.e@gmail.com>
2020-06-01 13:37:37 -0500

    enabled remove button on Favorites page

--- src/Components/FavoriteCard.js
+++ src/Components/FavoriteCard.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const FavoriteCard = props => {
     const product = props.product
+
+    const handleRemove = () => {
+        props.removeFavorite(product.category, product.id)
+    }
+
     return(
         <div>
-            <img src={product.img_url} alt="product"/>
+            <img src={product.img_url} alt="product"/><br/>
+            {props.create && <button>Select</button>}
+            <button onClick={handleRemove}>Remove</button>
+            {/* {console.log(product)} */}
         </div>
     )
 }
--- src/Containers/FavoritesPage.js
+++ src/Containers/FavoritesPage.js
@@ -3,10 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 import FavoritesList from '../Components/FavoritesList';
 
 const FavoritesPage = () => {
+
+    const myFavorites = props => {
+        return (
+            <div>
+                <h3>My Favorites</h3>
+                <FavoritesList {...props} />
+            </div>
+        )
+    }
+
     return(
         <div>
             <Switch>
-                <Route path='/myFavorites' component={FavoritesList} />
+                <Route path='/myFavorites' component={myFavorites} />
             </Switch>
         </div>
     )
