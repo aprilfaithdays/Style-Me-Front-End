bdd3f890a8aaffa5e31ab969bd5100b8ad682c0d
aprilfaithdays <aprilfaith.e@gmail.com>
2020-06-04 14:45:52 -0500

    able to login phase 1

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link} from 'react-router-dom'
+import { CurrentUserContext } from '../Containers/Store';
 
 
-const Login = () => {
+const Login = props => {
+    const [, setCurrentUser] = useContext(CurrentUserContext)
+    const [users, setUsers] = useState([])
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+
+    useEffect(()=> {
+        fetch('http://localhost:3000/users')
+        .then(res => res.json())
+        .then(res => setUsers(res))
+        // eslint-disable-next-line
+    },[])
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const user = users.find(user => user.username === username)
+        if (user) {
+            if(user.password === password){
+                const id = parseInt(user.id, 0)
+                setCurrentUser(id)
+                props.history.push('/')
+            } else {
+                alert("Can't find username/password combo")
+            }
+        } else {
+            alert("Can't find username/password combo")
+        }
+    }
 
     const returningUser = () => {
         return(
             <div>
                 <h5>Login</h5>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input className="form-control form-control-sm" type="text" placeholder="username" onChange={e => setUsername(e.target.value)} value={username}/>
                     <input className="form-control form-control-sm" type="password" placeholder="password" onChange={e => setPassword(e.target.value)} value={password}/>
                     <button className="btn btn-outline-secondary btn-sm">Login</button>
