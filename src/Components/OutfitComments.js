7b277be0118d7cf1a1af5c162812b2c5a16e1637
aprilfaithdays <aprilfaith.e@gmail.com>
2020-06-15 13:44:14 -0500

    able to delete comments completely

--- src/Components/OutfitComments.js
+++ src/Components/OutfitComments.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Moment from 'react-moment';
 import '../Styling/OutfitPage.css'
+import { useContext } from 'react';
+import { CurrentUserContext } from '../Context/CurrentUser';
 
 const OutfitComments = props => {
-
+    const [currentUser] = useContext(CurrentUserContext)
     
     const renderComments = () => {
         const list = props.comments
@@ -11,8 +14,21 @@ const OutfitComments = props => {
 
     const renderComment = comment =>{
         return (
-            <div key={comment.id}>
-                {comment.text}
+            <div key={comment.id} className="comments">
+                <img src={comment.user.img_url} alt={comment.user.name} className="comment-img"/>
+                <div className="cmt-text">
+                    <b>{comment.user.name}</b>
+                    <em className="cmt-created">
+                        <Moment fromNow>{comment.created_at}</Moment>
+                    </em>
+                    <div>
+                        {comment.text}
+                        {comment.user_id === currentUser.id && <button 
+                            className="btn btn-outline-secondary btn-sm"
+                            onClick={() => props.deleteComment(comment.id)}
+                        >Delete</button>}
+                    </div>
+                </div>
             </div>
         )
     }
