5ad130f432f9c630ca1963ea2d9958256071a73b
aprilfaithdays <aprilfaith.e@gmail.com>
2020-07-07 11:16:05 -0500

    fully functioning color filter on bottoms

--- src/Components/FilterOptionsForm.js
+++ src/Components/FilterOptionsForm.js
@@ -5,26 +5,14 @@ import Form from 'react-bootstrap/Form'
 const FilterOptionsForm = (props) => {
     const option = props.option
 
-    const handleChange = () => {
-        let updated = {
-            color: option.color, 
-            amount: option.amount, 
-            checked: !option.checked
-        };
-
-
-        props.checkFilter(updated)
-    }
-
     return(
         <div>
             <Form>
                 <div className="mb-3">
                     <Form.Check
                         type='checkbox'
-                        // checked={option.checked}
                         label={`${option.color} (${option.amount})`}
-                        onChange={handleChange}
+                        onChange={() => props.checkFilter(option)}
                     />
                 </div>
             </Form>
